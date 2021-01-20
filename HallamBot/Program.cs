6e6bd999d2c8829fc93241fe7e1a21be51d1b52c
= <me@alta.sh>
2021-01-20 07:53:21 +0000

    first commit

---
+++ HallamBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HallamBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
