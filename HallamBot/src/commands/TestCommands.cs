b22ff01dd9d50746e0ba05c6585729158ffd7e69
= <me@alta.sh>
2021-01-22 17:31:11 +0000

    code refactoring

--- HallamBot/src/commands/TestCommands.cs
+++ HallamBot/src/commands/TestCommands.cs
@@ -13,44 +13,29 @@ namespace HallamBot.Commands
     public class TestCommands : BaseCommandModule
     {
         [Command("test")]
-        public async Task Test(CommandContext ctx, string param = null, string param2 = null)
+        public async Task Test(CommandContext ctx, string typeOfTest = null)
         {
-            switch (param.ToLower())
+            if (typeOfTest == "alert")
             {
-                case "lecturealert":
-                    if (param2.ToLower() == "resolve")
-                        await LectureAlertTestResolve(ctx);
-                    else
-                        await LectureAlertTestCreate(ctx);
-                    break;
-                case null:
-                    await ctx.Channel.SendMessageAsync("This is a developer command for testing purposes.");
-                    break;
+                await ctx.Channel.SendMessageAsync("Generating a test lecture for testing purposes... will ping a Lecture Alert when ready.\nDon't forget to remove this test lecture once pinged with `LectureAlert Resovle`");
+                Models.Lecture testLecture = new Models.Lecture()
+                {
+                    Day = DateTime.Now,
+                    IsDropIn = false,
+                    Groups = "Test group data",
+                    IsTutorial = false,
+                    Lecturer = "Dr Test",
+                    ModuleName = "Test module",
+                    StartTime = DateTime.Now.TimeOfDay.Add(new TimeSpan(0, 30, 0))
+                };
+                TimetableData.Timetable.Lectures.Add(testLecture);
             }
-        }
-
-        private async Task LectureAlertTestResolve(CommandContext ctx)
-        {
-            var testLecture = TimetableData.Timetable.Lectures.Find(l => l.ModuleName.Equals("Test module"));
-            TimetableData.Timetable.Lectures.Remove(testLecture);
-
-            await ctx.Channel.SendMessageAsync($"Successfully removed {testLecture.ToString()}.");
-        }
-
-        private async Task LectureAlertTestCreate(CommandContext ctx)
-        {
-            Models.Lecture testLecture = new Models.Lecture()
+            else if (typeOfTest == "resolve")
             {
-                Day = DateTime.Now,
-                IsDropIn = false,
-                Groups = "Test group data",
-                IsTutorial = false,
-                Lecturer = "Dr Test",
-                ModuleName = "Test module",
-                StartTime = DateTime.Now.TimeOfDay.Add(new TimeSpan(0, 30, 0))
-            };
-            TimetableData.Timetable.Lectures.Add(testLecture);
-            await ctx.Channel.SendMessageAsync("Generating a test lecture for testing purposes... will ping a Lecture Alert when ready.\nDon't forget to remove this test lecture once pinged with `LectureAlert Resovle`");
+                Models.Lecture testLecture = TimetableData.Timetable.Lectures.Find(l => l.ModuleName.Equals("Test module"));
+                await ctx.Channel.SendMessageAsync($"Successfully removed {testLecture.ToString()}.");
+                TimetableData.Timetable.Lectures.Remove(testLecture);
+            }
         }
     }
 }
